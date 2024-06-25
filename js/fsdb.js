53d487a9c219f0f5c1c625af1702e9a25d8b6079
ProCodersEg <66762184+ProCodersEg@users.noreply.github.com>
2024-06-25 16:45:46 +0300

    Update fsdb.js

--- js/fsdb.js
+++ js/fsdb.js
@@ -272,6 +272,9 @@ function sendNotificationToUser(userId) {
                             title: "سر جديد",
                             body: "لقد استقبلت سر جديد اضغط للمعاينه",
 				channel_id: "channel_id",
+				priority : "high",
+				sound:"default",
+				largeIcon: 'icon',
 				icon: "icon" // Use the correct small icon name
                         }
                     };
