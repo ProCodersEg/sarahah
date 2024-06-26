833bf7df1ee824b9327dde98689ca11475453b60
ProCodersEg <66762184+ProCodersEg@users.noreply.github.com>
2024-06-26 04:22:25 +0300

    Update fsdb.js

--- js/fsdb.js
+++ js/fsdb.js
@@ -272,14 +272,9 @@ function sendNotificationToUser(userId) {
               title: "سر جديد",
               body: "لقد استقبلت سر جديد اضغط للمعاينه",
               channel_id: "channel_id",
-              icon: "icon" // Use the correct small icon name
-              //image: "https://www.sarhne.com/blog/media/2021-11-27-2149488.webp", // URL to the large icon
+              icon: "icon", // Use the correct small icon name
+              image: "https://www.sarhne.com/blog/media/2021-11-27-2149488.webp" // URL to the large icon
             },
-		  android: {
-              notification: {
-                largeIcon: "https://www.sarhne.com/blog/media/2021-11-27-2149488.webp" // URL to the large icon
-              }
-            }
           };
           sendNotification(notification);
         } else {
