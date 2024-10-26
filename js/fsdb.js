29213e0702d971b2a3116305c67d0913fc5481e8
ProCodersEg <66762184+ProCodersEg@users.noreply.github.com>
2024-10-27 01:26:59 +0300

    Update ad.js

--- js/fsdb.js
+++ js/fsdb.js
@@ -264,18 +264,22 @@ function sendNotificationToUser(userId) {
     .then(doc => {
       if (doc.exists) {
         const { token: userFCMToken, notificationsOn } = doc.data();
-
         if (notificationsOn === true && userFCMToken) {
           const notification = {
             to: userFCMToken,
             notification: {
               title: "سر جديد",
               body: "لقد استقبلت سر جديد اضغط للمعاينه",
-              channel_id: "channel_id",
-              icon: "icon", // Use the correct small icon name
-              image: "https://www.sarhne.com/blog/media/2021-11-27-2149488.webp" // URL to the large icon
+              // channel_id is defined in your Notification Channel on the Android side
+              channel_id: "channel_id", 
+              icon: "icon", // This should match your drawable resource name
             },
+            data: {
+              image: "https://www.sarhne.com/blog/media/2021-11-27-2149488.webp", // This is data payload
+              additionalData: "any additional data you need" // Any other data you want to send
+            }
           };
+
           sendNotification(notification);
         } else {
           console.log("Notifications are turned off for the user or FCM token not found.");
@@ -288,6 +292,7 @@ function sendNotificationToUser(userId) {
       console.error("Error fetching user document:", error);
     });
 }
+
 
 
 
